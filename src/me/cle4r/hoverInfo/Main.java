aa4b04db361b4823a3ff1d9f4c218bc962293936
cle4r <nathanball@tutanota.com>
2020-10-19 21:47:37 +1100

    Created main.java class

---
+++ src/me/cle4r/hoverInfo/Main.java
@@ -0,0 +1,16 @@
+package me.cle4r.hoverInfo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		// On startup this will run
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
